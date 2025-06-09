86423aed8fb19d2d5e3bf741c254ca8375bf9659
CJay-Cipher <chijiokennabueze@gmail.com>
2025-06-09 10:20:54 +0100

    responsiveness update for all screens

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Import your page components
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
