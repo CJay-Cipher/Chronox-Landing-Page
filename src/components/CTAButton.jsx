7f0e25f5b0ddafe6d2af07c39099739eef2cce6b
CJay-Cipher <chijiokennabueze@gmail.com>
2025-06-11 14:37:50 +0100

    MobileDropdown added and tested

--- src/components/CTAButton.jsx
+++ src/components/CTAButton.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-const CTAButton = ({ btnText = "Get started", className = "" }) => {
+const CTAButton = ({ btnText = "Get Started", href = "/get-started", className = "", isDarkBg = true }) => {
   return (
     <a
-      href="/get-started"
-      className={`text-center text-nowrap font-semibold xl:text-[18px] lg:text-[16px] text-[14px] bg-[#24CDAF] text-white px-8 py-2 rounded-[30px] hover:bg-[#009B8C] transition ${className}`}
+      href={href}
+      className={`text-center text-nowrap font-semibold xl:text-[18px] lg:text-[16px] text-[14px] px-8 py-2 border border-[#24CDAF] rounded-[30px] transition ${className} ${
+        isDarkBg ? "bg-[#24CDAF] text-white hover:bg-[#009B8C]" : "bg-white text-[#24CDAF] hover:text-white hover:bg-[#009B8C]"
+      }`}
     >
       {btnText}
     </a>
